1ac511651f67dc1e84179998733627e3d544d4a1
tejasri3123 <nallatejasri@klh.edu.in>
2026-06-13 17:10:48 +0530

    Create App.jsx

---
+++ Week12/App.jsx
@@ -0,0 +1,13 @@
+import BookingForm from "./components/BookingForm";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>✈️ Airline Booking Application</h1>
+      <BookingForm />
+    </div>
+  );
+}
+
+export default App;
